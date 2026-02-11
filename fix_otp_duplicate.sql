700ba71a88e9b3ab2eb86ab0db5db1413bfc4c61
RIN <aldrinisidro60@gmail.com>
2026-02-12 00:47:35 +0800

    .

--- fix_otp_duplicate.sql
+++
@@ -1,4 +0,0 @@
--- Remove duplicate entry with id = 0
-DELETE FROM otp_codes WHERE id = 0;
--- Reset auto-increment value
-ALTER TABLE otp_codes AUTO_INCREMENT = 1;
--- gen_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Temporary helper: outputs bcrypt hash for a chosen password
-// Usage: http://localhost/ERS/gen_hash.php?password=admin11
-
-header('Content-Type: text/plain');
-$pwd = isset($_GET['password']) ? (string)$_GET['password'] : 'admin11';
-echo password_hash($pwd, PASSWORD_DEFAULT);
