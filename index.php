1a20736c51bf84eebecffebb77b48d8670963482
Lloyd <lloydsamonte7@gmail.com>
2026-02-28 00:51:18 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+require_once __DIR__ . '/includes/auth.php'; // optional kung meron
+
 
 $pageTitle = 'ERS Admin Dashboard';
 $activeIncidents = 0;
